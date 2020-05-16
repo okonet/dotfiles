ff91c304ce56aeca51be1a0bc6f7110b1b46e1dc
Andrey Okonetchnikov <andrey@okonet.ru>
2020-05-16 13:55:26 +0200

    Add brisync

--- applications.sh
+++ applications.sh
@@ -39,6 +39,7 @@ brew cask install figma
 brew cask install audirvana-plus2
 brew cask install imageoptim
 brew cask install imagealpha
+brew cask install brisync
 
 # Support older versions. This is needed for Lightroom
 # brew tap caskroom/homebrew-versions
